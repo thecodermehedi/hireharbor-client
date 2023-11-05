a58281131adc654a28065729fe2aef7d56b4158f
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-06 01:06:48 +0600

    hooks: create useAuth.jsx and useAxios.jsx

---
+++ src/hooks/useAuth.jsx
@@ -0,0 +1,12 @@
+
+
+const useAuth = () => {
+  return (
+    <div>
+      <h1>useAuth</h1>
+      
+    </div>
+  );
+};
+
+export default useAuth;
---
+++ src/hooks/useAxios.jsx
@@ -0,0 +1,12 @@
+
+
+const useAxios = () => {
+  return (
+    <div>
+      <h1>useAxios</h1>
+      
+    </div>
+  );
+};
+
+export default useAxios;
