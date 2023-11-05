6de835d2e7c5d6213d0a35c576c72c42b1d54228
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-06 01:06:00 +0600

    feat: create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import Subscribe from "../components/ui/Subscribe";
+const Home = () => {
+  return (
+    <section className="mx-auto container">
+      <Subscribe />
+    </section>
+  );
+};
+
+export default Home;
