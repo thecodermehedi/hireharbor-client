5b5c394dc7601e92273387a5a3ff11d6cff699c7
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-06 23:26:13 +0600

    Refactor Jobs page to use JobsBanner and JobsTable components

--- src/pages/Jobs.jsx
+++ src/pages/Jobs.jsx
@@ -1,9 +1,20 @@
-const AllJobs = () => {
+import {useState} from "react";
+import JobsBanner from "../components/ui/JobsBanner";
+import JobsTable from "../components/ui/JobsTable";
+
+const Jobs = () => {
+  const [searchFieldValue, setSearchFieldValue] = useState("");
+
+  const handleJobSearch = (e) => {
+    e.preventDefault();
+    setSearchFieldValue(e.target.search.value);
+  };
   return (
-    <div>
-      <h1>AllJobs</h1>
-    </div>
+    <section className="container mx-auto">
+      <JobsBanner handleJobSearch={handleJobSearch} />
+      <JobsTable searchFieldValue={searchFieldValue} />
+    </section>
   );
 };
 
-export default AllJobs;
+export default Jobs;
