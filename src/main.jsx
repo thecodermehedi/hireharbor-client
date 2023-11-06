d022312b36544a9aa73c2f67804da36b56936caf
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-06 23:15:32 +0600

    Added react-query and routing in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,17 @@ import ReactDOM from "react-dom/client";
 import {RouterProvider} from "react-router-dom";
 import router from "./routes";
 import "./index.css";
+import AuthProvider from "./providers/AuthProvider";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
