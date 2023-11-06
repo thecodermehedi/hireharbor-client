bf1870d2ba52affbb1e8a0d2d37afb99188c30e1
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-06 23:23:57 +0600

    Add banner and job categories components to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
+import Banner from "../components/ui/Banner";
+import JobCategories from "../components/ui/JobCategories";
 import Subscribe from "../components/ui/Subscribe";
 const Home = () => {
   return (
-    <section className="mx-auto container">
+    <section>
+      <Banner />
+      <JobCategories />
       <Subscribe />
     </section>
   );
