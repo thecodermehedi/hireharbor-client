cde24f2e2ffd9392f5c231006529a293d7f8c95f
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-08 20:13:47 +0600

    Add HelmetProvider to main.jsx.

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,18 @@ import router from "./routes";
 import "./index.css";
 import AuthProvider from "./providers/AuthProvider";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {HelmetProvider} from "react-helmet-async";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <RouterProvider router={router} />
-      </AuthProvider>
+      <HelmetProvider>
+        <AuthProvider>
+          <RouterProvider router={router} />
+        </AuthProvider>
+      </HelmetProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
