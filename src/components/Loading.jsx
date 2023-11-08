0fcfb37e2e11d5bdba88e46ffbfba454c5d7ba81
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-08 20:14:10 +0600

    Add Loading component.

---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+function Loading() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-blackish">
+      <span className="loading loading-ring loading-lg text-primary/75"></span>
+    </div>
+  );
+}
+
+export default Loading;
