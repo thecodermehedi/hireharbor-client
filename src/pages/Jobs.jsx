df772e9cd6827124d9589cad527474d35e539730
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-08 20:15:19 +0600

    Add meta tags for Jobs page

--- src/pages/Jobs.jsx
+++ src/pages/Jobs.jsx
@@ -1,6 +1,7 @@
 import {useState} from "react";
 import JobsBanner from "../components/ui/JobsBanner";
 import JobsTable from "../components/ui/JobsTable";
+import { Helmet } from "react-helmet-async";
 
 const Jobs = () => {
   const [searchFieldValue, setSearchFieldValue] = useState("");
@@ -11,6 +12,13 @@ const Jobs = () => {
   };
   return (
     <section className="container mx-auto">
+      <Helmet>
+        <title>Jobs - HireHarbor</title>
+        <meta
+          name="description"
+          content="Explore jobs and apply for them."
+        />
+      </Helmet>
       <JobsBanner handleJobSearch={handleJobSearch} />
       <JobsTable searchFieldValue={searchFieldValue} />
     </section>
