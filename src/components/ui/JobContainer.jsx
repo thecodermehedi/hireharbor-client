0fd1a726bf8d9556da811119574fbf711df3ed46
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-08 20:14:37 +0600

    Remove console.log statement from JobContainer component

--- src/components/ui/JobContainer.jsx
+++ src/components/ui/JobContainer.jsx
@@ -2,7 +2,6 @@ import PropTypes from "prop-types";
 import JobCard from "./JobCard";
 
 const JobContainer = ({jobs}) => {
-  // console.log(jobs);
   return (
     <section className=" my-10 mr-12 lg:mr-0">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 place-items-center">
