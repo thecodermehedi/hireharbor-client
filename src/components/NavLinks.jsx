71aebc221cf4167a88a3f23a05813be7f9a191bd
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-08 20:14:16 +0600

    Add useAuth hook to NavLinks component

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -1,6 +1,7 @@
 import {NavLink} from "react-router-dom";
-
+import useAuth from "../hooks/useAuth";
 const NavLinks = () => {
+  const {user} = useAuth();
   return (
     <>
       <NavLink
@@ -23,36 +24,41 @@ const NavLinks = () => {
       >
         All Jobs
       </NavLink>
-      <NavLink
-        to={"appliedjobs"}
-        className={({isActive}) =>
-          isActive
-            ? "active text-primary"
-            : "text-whitish transition hover:text-primary"
-        }
-      >
-        Applied Jobs
-      </NavLink>
-      <NavLink
-        to={"addjob"}
-        className={({isActive}) =>
-          isActive
-            ? "active text-primary"
-            : "text-whitish transition hover:text-primary"
-        }
-      >
-        Add A Job
-      </NavLink>
-      <NavLink
-        to={"postedjobs"}
-        className={({isActive}) =>
-          isActive
-            ? "active text-primary"
-            : "text-whitish transition hover:text-primary"
-        }
-      >
-        My Jobs
-      </NavLink>
+      {user?.email && (
+        <>
+          {" "}
+          <NavLink
+            to={"appliedjobs"}
+            className={({isActive}) =>
+              isActive
+                ? "active text-primary"
+                : "text-whitish transition hover:text-primary"
+            }
+          >
+            Applied Jobs
+          </NavLink>
+          <NavLink
+            to={"addjob"}
+            className={({isActive}) =>
+              isActive
+                ? "active text-primary"
+                : "text-whitish transition hover:text-primary"
+            }
+          >
+            Add A Job
+          </NavLink>
+          <NavLink
+            to={"postedjobs"}
+            className={({isActive}) =>
+              isActive
+                ? "active text-primary"
+                : "text-whitish transition hover:text-primary"
+            }
+          >
+            My Jobs
+          </NavLink>
+        </>
+      )}
       <NavLink
         to={"blogs"}
         className={({isActive}) =>
