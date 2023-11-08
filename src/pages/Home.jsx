889d0c66770b89bb0303e684c23d8bcce87f8402
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-08 20:15:09 +0600

    Add meta tags and title to Home page.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,17 @@
+import {Helmet} from "react-helmet-async";
 import Banner from "../components/ui/Banner";
 import JobCategories from "../components/ui/JobCategories";
 import Subscribe from "../components/ui/Subscribe";
 const Home = () => {
   return (
     <section>
+      <Helmet>
+        <title>Home - HireHarbor</title>
+        <meta
+          name="description"
+          content="Welcome to HireHarbor. Explore job categories and subscribe for updates."
+        />
+      </Helmet>
       <Banner />
       <JobCategories />
       <Subscribe />
