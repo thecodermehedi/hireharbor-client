654224755e0cfefa5229394c6271bb90ce93506c
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-08 20:14:06 +0600

    Add PrivateRoute component with authentication logic

---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,24 @@
+import {Navigate, useLocation} from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+import PropTypes from "prop-types";
+import toast from "react-hot-toast";
+import Loading from "../components/Loading";
+
+const PrivateRoute = ({children}) => {
+  console.log("PrivateRoute rendered");
+  const {user, isLoading} = useAuth();
+  const location = useLocation();
+  if (isLoading) {
+    return <Loading />;
+  }
+  if (!isLoading && !user?.email) {
+    toast.error("You have to log in first to view details");
+    return <Navigate state={location.pathname} to="/login" replace />;
+  }
+  return children;
+};
+
+PrivateRoute.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+export default PrivateRoute;
